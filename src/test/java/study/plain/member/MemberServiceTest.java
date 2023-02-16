636e5132957d7367773fe61f97a553ce7c72ac2b
SW-H <hsw08@naver.com>
2023-02-16 16:11:35 +0900

    [ADD] JUnit test file for User

---
+++ src/test/java/study/plain/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package study.plain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "tester", Grade.VIP);
+        //when
+        memberService.join(member);
+        Member foundMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(foundMember);
+
+    }
+}
